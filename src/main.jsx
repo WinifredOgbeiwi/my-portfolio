cecc299d752143a9a9a7a3d3d8f2a66884dbdd89
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-09-11 04:25:14 +0200

    my portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-WVT1NWPFZJ");
+
+ReactGA.send({ hitType: "pageview", page: "window.location.pathname"});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
